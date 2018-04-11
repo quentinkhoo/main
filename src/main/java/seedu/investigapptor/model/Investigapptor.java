f23792c06ff765a7bb3efcfc17d677d108b53e9a
Marcus Chen <Marcus_cxc@outlook.com>
2018-04-11 14:05:54 +0800

    Update the EditInvestigatorCommand as well as the model component

--- src/main/java/seedu/investigapptor/model/Investigapptor.java
+++ src/main/java/seedu/investigapptor/model/Investigapptor.java
@@ -148,14 +148,33 @@ public class Investigapptor implements ReadOnlyInvestigapptor {
     public void updatePerson(Person target, Person editedPerson)
             throws DuplicatePersonException, PersonNotFoundException {
         requireNonNull(editedPerson);
-
-        Person syncedEditedPerson = syncWithMasterTagList(editedPerson);
         // TODO: the tags master list will be updated even though the below line fails.
         // This can cause the tags master list to have additional tags that are not tagged to any person
         // in the person list.
+        for (CrimeCase c :((Investigator) target).getCrimeCases()) {
+            Investigator inv = new Investigator(editedPerson.getName(), editedPerson.getPhone(),
+                    editedPerson.getEmail(), editedPerson.getAddress(), ((Investigator) editedPerson).getRank(),
+                    editedPerson.getTags());
+            CrimeCase newCase = new CrimeCase(c.getCaseName(), c.getDescription(), inv, c.getStartDate(),
+                    c.getEndDate(), c.getStatus(), c.getTags());
+            try {
+                cases.remove(c);
+                addCrimeCase(newCase);
+            } catch (Exception e) {
+                throw new AssertionError("Case does not exist");
+            }
+        }
+        Person syncedEditedPerson = syncWithMasterTagList(editedPerson);
         persons.setPerson(target, syncedEditedPerson);
     }
+    /**
+     * Removes {@code key} from this {@code Investigapptor}.
+     *
+     * @throws PersonNotFoundException if the {@code key} is not in this {@code Investigapptor}.
+     */
+    public void updateCasesInvestigator (Investigator updatedInvestigator) {
 
+    }
     /**
      * Removes {@code key} from this {@code Investigapptor}.
      *
